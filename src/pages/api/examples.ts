d3982392b3b46887ceb78e41d24a801bf5ca74c9
Camden Withrow <cmwithrow@anderson.edu>
2022-08-13 17:12:12 -0400

    remove more example

--- src/pages/api/examples.ts
+++ src/pages/api/examples.ts
@@ -4,7 +4,7 @@ import { prisma } from "../../server/db/client";
 
 const examples = async (req: NextApiRequest, res: NextApiResponse) => {
   // const examples = await prisma.example.findMany();
-  res.status(200).json(examples);
+  // res.status(200).json(examples);
 };
 
 export default examples;
